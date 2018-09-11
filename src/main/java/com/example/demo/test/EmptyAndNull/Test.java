ede55adb1bae784c3134fb77c6ffb6b33e9e15bf
liujie <http://10.0.200.18/MIB/parking-log-analysis-platform.git>
2018-09-11 18:04:09 +0800

    初始化

--- src/main/java/com/example/demo/test/EmptyAndNull/Test.java
+++ src/main/java/com/example/demo/test/EmptyAndNull/Test.java
@@ -15,6 +15,6 @@ public class Test {
         String str = "";
         System.out.println(!str.isEmpty());
         System.out.println(str != null);
-        System.out.println("111");
+        System.out.println("1231");
     }
 }
