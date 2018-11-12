df43f2702449fb6fa8a4c1eb48471791a32d089b
liujie <http://10.0.200.18/MIB/parking-log-analysis-platform.git>
2018-11-12 14:34:08 +0800

    try-catch-finally中的相关注意事项

--- src/main/java/com/example/demo/test/tryCarch/TryCatchTest.java
+++ src/main/java/com/example/demo/test/tryCarch/TryCatchTest.java
@@ -19,7 +19,7 @@ public class TryCatchTest {
         }catch (Exception e){
             return ++a;
         }finally {
-            System.out.println(a);;
+            System.out.println(a);
         }
         return 0;
     }
@@ -27,4 +27,14 @@ public class TryCatchTest {
     public static void main(String[] args){
         System.out.println(tryTest(5));
     }
+
+    /**
+     *  （1）  当java程序执行try、catch块遇到return语句，会先执行return语句，执行return之后，不会立即返回结束方法，
+     * 而是去寻找该异常处理流程中是否包含finally语句块，如果没有，方法终止，返回相应值；如果存在，系统立即执行finally
+     * 块，如果finally块中没有return语句，系统才会再次调回前面的return语句结束方法；如果finally中有return语句，则会
+     * 直接结束方法，不会再跳回去执行
+     * （2）   对于throw new RuntimeException()，在catch中执行throw语句时并不会立即抛出异常，而是立即去寻找是否有
+     * finally块；如果没有就立即抛出，如果有就先执行finally语句块，如果finally中没有return，则会跳转回来执行throw，
+     * 否则finally直接return，结束方法。
+     */
 }
