cf396cd4fa1c05d9f570023db95ea7d8a91e1efc
liujie <http://10.0.200.18/MIB/parking-log-analysis-platform.git>
2018-11-29 17:59:42 +0800

    快速排序相关说明与改进

--- src/main/java/com/example/demo/test/dataStructureAndArithmetic/sortArithmetic/QuickSort3.java
+++ src/main/java/com/example/demo/test/dataStructureAndArithmetic/sortArithmetic/QuickSort3.java
@@ -15,19 +15,21 @@ import java.util.Arrays;
 
 public class QuickSort3 {
 
+    //(1)时间复杂度为O(log(N))
+    //(2)不稳定（发生在基准元素与a[left]交换时）
     private static int getMiddle(int[] a, int left, int right) {
         int key = a[left];
         while (left < right) {
             while (left < right && a[right] >= key) {
                 right--;
             }
-            if(left<right) {
+            if (left < right) {
                 a[left] = a[right];
             }
             while (left < right && a[left] <= key) {
                 left++;
             }
-            if(left<right) {
+            if (left < right) {
                 a[right] = a[left];
             }
         }
@@ -35,49 +37,19 @@ public class QuickSort3 {
         return left;
     }
 
-    private static void quickSort1(int[] a,int left,int right){
-        int middle = 0;
-        if(left<right){
-            middle = getMiddle(a,left,right);
-            quickSort(a,left,middle-1);
-            quickSort(a,middle+1,right);
+    private static void quickSort1(int[] a, int left, int right) {
+        int middle;
+        if (left < right) {
+            middle = getMiddle(a, left, right);
+            quickSort1(a, left, middle - 1);
+            quickSort1(a, middle + 1, right);
         }
     }
 
     public static void main(String[] args) {
-
-        int [] arr = {6,1,2,7,9,13,4,5,10,8,3,33,21};
-        System.out.println("排序前："+ Arrays.toString(arr));
-        quickSort1(arr,0,arr.length-1);
-        System.out.println("排序后："+ Arrays.toString(arr));
-
+        int[] arr = {6, 1, 2, 7, 9, 13, 4, 5, 10, 8, 3, 33, 21};
+        System.out.println("排序前：" + Arrays.toString(arr));
+        quickSort1(arr, 0, arr.length - 1);
+        System.out.println("排序后：" + Arrays.toString(arr));
     }
-
-    public static void quickSort(int [] arr,int left,int right) {
-        int pivot = 0;
-        if(left < right) {
-            pivot = partition(arr,left,right);
-            quickSort(arr,left,pivot-1);
-            quickSort(arr,pivot+1,right);
-        }
-    }
-
-    private static int partition(int[] arr,int left,int right) {
-
-        int key = arr[left];
-        while(left < right) {
-            while(left < right && arr[right] >= key) {
-                right--;
-            }
-            arr[left] = arr[right];
-            while(left < right && arr[left] <= key) {
-                left++;
-            }
-            arr[right] = arr[left];
-        }
-        arr[left] = key;
-        return left;
-
-    }
-
 }
