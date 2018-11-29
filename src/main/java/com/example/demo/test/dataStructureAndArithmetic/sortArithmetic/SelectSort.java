4ef9ea5db8735aeff3074660f686ff8dec3d2145
liujie <http://10.0.200.18/MIB/parking-log-analysis-platform.git>
2018-11-29 17:53:32 +0800

    选择排序相关说明与改进

--- src/main/java/com/example/demo/test/dataStructureAndArithmetic/sortArithmetic/SelectSort.java
+++ src/main/java/com/example/demo/test/dataStructureAndArithmetic/sortArithmetic/SelectSort.java
@@ -13,16 +13,22 @@ package com.example.demo.test.dataStructureAndArithmetic.sortArithmetic;
 
 public class SelectSort {
 
-    private static void selectSort(int[] a){
-        for(int i =0;i<a.length - 1;i++){
-            for(int j = i+1;j<a.length;j++){
-                swap(a,i,j);
+    /**
+     * 时间复杂度为O(N^2)，不稳定
+     * 如：5,28,5,2第一次排序5和2交换位置就破坏了稳定
+     *
+     * @param a 待排序数组
+     */
+    private static void selectSort(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                swap(a, i, j);
             }
         }
     }
 
-    private static void swap(int[] a,int m,int n){
-        if(a[m] > a[n]){
+    private static void swap(int[] a, int m, int n) {
+        if (a[m] > a[n]) {
             int temp = a[m];
             a[m] = a[n];
             a[n] = temp;
@@ -30,10 +36,10 @@ public class SelectSort {
     }
 
     public static void main(String[] args) {
-        int[] a = {11,12,45,3,67,66,36};
+        int[] a = {11, 12, 45, 3, 67, 66, 36};
         selectSort(a);
-        for(int i =0;i<a.length;i++){
-            System.out.print(a[i]+" ");
+        for (int num : a) {
+            System.out.print(num + " ");
         }
     }
 
