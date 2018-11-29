ded6dde3b553fc61b229a32c7a6da6a3264a06a6
liujie <http://10.0.200.18/MIB/parking-log-analysis-platform.git>
2018-11-29 17:48:20 +0800

    冒泡排序相关说明与改进

--- src/main/java/com/example/demo/test/dataStructureAndArithmetic/sortArithmetic/BubbleSort.java
+++ src/main/java/com/example/demo/test/dataStructureAndArithmetic/sortArithmetic/BubbleSort.java
@@ -14,19 +14,20 @@ package com.example.demo.test.dataStructureAndArithmetic.sortArithmetic;
 public class BubbleSort {
 
     /**
-     *  时间复杂度为O(N^2)
+     * 时间复杂度为O(N^2),稳定
+     *
      * @param a 要排序的数组
      */
-    private static void bubbleSort(int[] a){
-        for(int i =1;i<a.length;i++){     //外层循环控制排序趟数
-            for(int j = 0;j<a.length-1;j++){   //内层循环控制每一趟排序多少次
-                swap(a,j,j+1);
+    private static void bubbleSort(int[] a) {
+        for (int i = 1; i < a.length; i++) {     //外层循环控制排序趟数
+            for (int j = 0; j < a.length - 1; j++) {   //内层循环控制每一趟排序多少次
+                swap(a, j, j + 1);
             }
         }
     }
 
-    private static void swap(int[] a,int m,int n){
-        if(a[m] > a[n]){
+    private static void swap(int[] a, int m, int n) {
+        if (a[m] > a[n]) {
             int temp = a[m];
             a[m] = a[n];
             a[n] = temp;
@@ -34,10 +35,10 @@ public class BubbleSort {
     }
 
     public static void main(String[] args) {
-        int[] a = {1,4,7,19,20,45,3};
+        int[] a = {1, 4, 7, 19, 20, 45, 3};
         bubbleSort(a);
-        for(int i =0;i<a.length;i++){
-            System.out.print(a[i]+" ");
+        for (int num : a) {
+            System.out.print(num + " ");
         }
     }
 }
