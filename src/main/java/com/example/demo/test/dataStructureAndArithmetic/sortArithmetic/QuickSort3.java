44cec5735f08fb8a94a9eb48bd1bdbf43f51446a
liujie <http://10.0.200.18/MIB/parking-log-analysis-platform.git>
2018-12-11 15:52:44 +0800

    快速排序添加说明

--- src/main/java/com/example/demo/test/dataStructureAndArithmetic/sortArithmetic/QuickSort3.java
+++ src/main/java/com/example/demo/test/dataStructureAndArithmetic/sortArithmetic/QuickSort3.java
@@ -17,6 +17,14 @@ public class QuickSort3 {
 
     //(1)时间复杂度为O(log(N))
     //(2)不稳定（发生在基准元素与a[left]交换时）
+    //(3)不用进行两个元素的互换，以{6, 1, 2, 7, 9, 13, 4, 5, 10, 8}为例
+    //   初始：key=6,left=0,right=9
+    //   发生第一次改变时： key=6,left=0,right=7；变化后：{5,1,2,7,9,13,4,5,10,8}，消失的是基准元素key
+    //   第二次：key=6,left=3,right=7;改变后：{5,1,2,7,9,13,4,7,10,8}，原来right标识的值从5被覆盖为7
+    //   第三次：key=6,left=3,right=6;改变后：{5,1,2,4,9,13,4,7,10,8}，原来left标识的值从7被覆盖为4
+    //   第四次：key=6,left=4,right=6;改变后：{5,1,2,4,9,13,9,7,10,8}，原来right标识的值从4被覆盖为9
+    //   第五次：key=6,left=4,right=4,此时left < fight不再满足，跳出while循环，同时left标识的值被key覆盖
+    //        改变后:{5,1,2,4,6,13,9,7,10,8} ,key(6)左边的都比它小，右边的都比它大，在进行递归
     private static int getMiddle(int[] a, int left, int right) {
         int key = a[left];
         while (left < right) {
@@ -37,19 +45,19 @@ public class QuickSort3 {
         return left;
     }
 
-    private static void quickSort1(int[] a, int left, int right) {
+    private static void quickSort(int[] a, int left, int right) {
         int middle;
         if (left < right) {
             middle = getMiddle(a, left, right);
-            quickSort1(a, left, middle - 1);
-            quickSort1(a, middle + 1, right);
+            quickSort(a, left, middle - 1);
+            quickSort(a, middle + 1, right);
         }
     }
 
     public static void main(String[] args) {
-        int[] arr = {6, 1, 2, 7, 9, 13, 4, 5, 10, 8, 3, 33, 21};
+        int[] arr = {6, 1, 2, 7, 9, 13, 4, 5, 10, 8};
         System.out.println("排序前：" + Arrays.toString(arr));
-        quickSort1(arr, 0, arr.length - 1);
+        quickSort(arr, 0, arr.length - 1);
         System.out.println("排序后：" + Arrays.toString(arr));
     }
 }
