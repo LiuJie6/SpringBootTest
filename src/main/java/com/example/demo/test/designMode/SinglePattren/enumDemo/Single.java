18017cb6290a48d9f613f812ec8b281d381070ee
liujie <http://10.0.200.18/MIB/parking-log-analysis-platform.git>
2019-02-15 10:48:20 +0800

    枚举实现单例模式（未完成）

---
+++ src/main/java/com/example/demo/test/designMode/SinglePattren/enumDemo/Single.java
@@ -0,0 +1,17 @@
+package com.example.demo.test.designMode.SinglePattren.enumDemo;
+
+/**
+ * Project Name:SpringBootTest
+ * File Name:Single
+ * Package Name:com.example.demo.test.designMode.SinglePattren.enumDemo
+ * Date:2019/2/14
+ * Author:liujie
+ * Description:
+ * Copyright (c) 2019, 重庆云凯科技有限公司 All Rights Reserved.
+ */
+
+
+public enum Single {
+
+
+}
